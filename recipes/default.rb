eae32bff68578ebeaf3753b820696cd1484b71d9
Samuel Bernard <samuel.bernard@s4m.io>
2015-05-19 16:22:34 +0200

    Generate config and id from role search

--- recipes/default.rb
+++ recipes/default.rb
@@ -2,4 +2,30 @@
 # Cookbook Name:: zookeeper-cluster
 # Recipe:: default
 #
-# Copyright (c) 2015 The Authors, All Rights Reserved.
+# Copyright (c) 2015 S4M, All Rights Reserved.
+
+puts "\nStarting #{cookbook_name}::#{recipe_name}"
+
+# Fetch role name of the cluster
+cluster_role = node['zookeeper-cluster']['role']
+
+# Fetch hosts based on a search
+fqdn = node['fqdn']
+puts "Searching in role #{cluster_role}"
+hosts = search(:node, "roles:#{cluster_role}")
+hosts = hosts.collect {|n| n['fqdn']}
+hosts << fqdn unless hosts.include? fqdn
+hosts = hosts.sort
+puts "Hosts found: #{hosts}"
+
+# My ID
+my_id = (hosts.index fqdn)+1
+puts "My ID: #{my_id}"
+
+# Generate config
+node_config = node['zookeeper-cluster']['config']
+config = {}.merge node_config
+hosts.each_with_index { |v,i| config["server.#{i+1}"]="#{v}:2888:3888" }
+puts "Configuration: #{config}"
+
+puts "End of #{cookbook_name}::#{recipe_name}\n\n"
